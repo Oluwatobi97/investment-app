4c3b1d45adc406e932fadece7f15730db5aa7a50
Oluwatobi97 <victortobi2000@gamil.com>
2024-11-24 00:22:51 +0100

    request-change

--- src/context/userContext/UserContext.jsx
+++ src/context/userContext/UserContext.jsx
@@ -1,7 +1,13 @@
 //  i want to get the logged in user
 
 //  work on this log-in system more
-import { createContext, useContext, useEffect, useState } from "react";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { ApiRequest } from "../../lib/data/makeRequest";
 import { useNavigate } from "react-router-dom";
 
@@ -9,39 +15,6 @@ const userContext = createContext(null);
 
 const STORAGE_KEY = "logged-in";
 const USER_STORAGE_KEY = "user-info";
-
-const useGetUserDetails = (isLoggedIn) => {
-  const [userDetails, setUserDetails] = useState(
-    localStorage.getItem(USER_STORAGE_KEY)
-      ? JSON.parse(localStorage.getItem(USER_STORAGE_KEY))
-      : null
-  );
-  const fetchLoggedInUser = async () => {
-    if (isLoggedIn) {
-      const token = localStorage.getItem("token");
-      const queryParams = new URLSearchParams({ token: token });
-      const result = await ApiRequest.GET(
-        `auth-system/authenticated-user?${queryParams.toString()}`
-      );
-
-      // setUserDetails({ ...result })
-      if (result.message === "un-Authorized") {
-        localStorage.removeItem(USER_STORAGE_KEY);
-        return;
-      }
-      setUserDetails(result);
-      localStorage.setItem(USER_STORAGE_KEY, JSON.stringify({ ...result }));
-      return;
-    }
-    localStorage.removeItem(USER_STORAGE_KEY);
-    return;
-  };
-
-  useEffect(() => {
-    fetchLoggedInUser(isLoggedIn);
-  }, [isLoggedIn]);
-  return { userDetails, setUserDetails };
-};
 
 export const UserContextProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(
