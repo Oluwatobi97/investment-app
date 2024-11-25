351e9c26beced65e6f2ea55d8a16761554e4c10f
Oluwatobi97 <victortobi2000@gamil.com>
2024-11-25 21:50:41 +0100

    trading

--- src/views/auth/shared/AboutUi/TradingGrap1.jsx
+++ src/views/auth/shared/AboutUi/TradingGrap1.jsx
@@ -1,123 +1,63 @@
-import React, { useEffect } from "react";
+// TradingViewWidget.jsx
+import React, { useEffect, useRef, memo } from "react";
 
 export const TradingGrap1 = () => {
+  const container = useRef();
+
   useEffect(() => {
     const script = document.createElement("script");
     script.src =
-      "https://s3.tradingview.com/external-embedding/embed-widget-market-overview.js";
+      "https://s3.tradingview.com/external-embedding/embed-widget-symbol-overview.js";
+    script.type = "text/javascript";
     script.async = true;
-
-    // Widget configuration
-    script.innerHTML = JSON.stringify({
-      title: "Cryptocurrencies",
-      tabs: [
-        {
-          title: "Overview",
-          symbols: [
-            { s: "CRYPTOCAP:TOTAL" },
-            { s: "BITSTAMP:BTCUSD" },
-            { s: "BITSTAMP:ETHUSD" },
-            { s: "FTX:SOLUSD" },
-            { s: "BINANCE:AVAXUSD" },
-            { s: "COINBASE:UNIUSD" },
+    script.innerHTML = `
+      {
+          "symbols": [
+            [
+              "FRED:SP500|1D"
+            ],
+            [
+              "FRED:NDQ100|1D"
+            ],
+            [
+              "BLACKBULL:US30|1D"
+            ]
           ],
-          quick_link: {
-            title: "More cryptocurrencies",
-            href: "/markets/cryptocurrencies/prices-all/",
-          },
-        },
-        {
-          title: "Bitcoin",
-          symbols: [
-            { s: "BITSTAMP:BTCUSD" },
-            { s: "COINBASE:BTCEUR" },
-            { s: "COINBASE:BTCGBP" },
-            { s: "BITFLYER:BTCJPY" },
-            { s: "CME:BTC1!" },
-          ],
-          quick_link: {
-            title: "More Bitcoin pairs",
-            href: "/symbols/BTCUSD/markets/",
-          },
-        },
-        {
-          title: "Ethereum",
-          symbols: [
-            { s: "BITSTAMP:ETHUSD" },
-            { s: "KRAKEN:ETHEUR" },
-            { s: "COINBASE:ETHGBP" },
-            { s: "BITFLYER:ETHJPY" },
-            { s: "BINANCE:ETHBTC" },
-            { s: "BINANCE:ETHUSDT" },
-          ],
-          quick_link: {
-            title: "More Ethereum pairs",
-            href: "/symbols/ETHUSD/markets/",
-          },
-        },
-        {
-          title: "Solana",
-          symbols: [
-            { s: "FTX:SOLUSD" },
-            { s: "BINANCE:SOLEUR" },
-            { s: "COINBASE:SOLGBP" },
-            { s: "BINANCE:SOLBTC" },
-            { s: "HUOBI:SOLETH" },
-            { s: "BINANCE:SOLUSDT" },
-          ],
-          quick_link: {
-            title: "More Solana pairs",
-            href: "/symbols/SOLUSD/markets/",
-          },
-        },
-        {
-          title: "Uniswap",
-          symbols: [
-            { s: "COINBASE:UNIUSD" },
-            { s: "KRAKEN:UNIEUR" },
-            { s: "COINBASE:UNIGBP" },
-            { s: "BINANCE:UNIBTC" },
-            { s: "KRAKEN:UNIETH" },
-            { s: "BINANCE:UNIUSDT" },
-          ],
-          quick_link: {
-            title: "More Uniswap pairs",
-            href: "/symbols/UNIUSD/markets/",
-          },
-        },
-      ],
-      width: "100%",
-      height: "100%",
-      showChart: true,
-      showFloatingTooltip: false,
-      locale: "en",
-      plotLineColorGrowing: "#2962FF",
-      plotLineColorFalling: "#2962FF",
-      belowLineFillColorGrowing: "rgba(41, 98, 255, 0.12)",
-      belowLineFillColorFalling: "rgba(41, 98, 255, 0.12)",
-      belowLineFillColorGrowingBottom: "rgba(41, 98, 255, 0)",
-      belowLineFillColorFallingBottom: "rgba(41, 98, 255, 0)",
-      gridLineColor: "rgba(240, 243, 250, 0)",
-      scaleFontColor: "rgba(120, 123, 134, 1)",
-      showSymbolLogo: true,
-      symbolActiveColor: "rgba(41, 98, 255, 0.12)",
-      colorTheme: "light",
-    });
-
-    // Append the script to the container
-    const container = document.querySelector(
-      ".tradingview-widget-container__widget"
-    );
-    container.appendChild(script);
-
-    // Cleanup script on component unmount
-    return () => {
-      container.innerHTML = "";
-    };
+          "chartOnly": false,
+          "width": 550,
+          "height": 400,
+          "locale": "en",
+          "colorTheme": "light",
+          "autosize": false,
+          "showVolume": false,
+          "showMA": false,
+          "hideDateRanges": false,
+          "hideMarketStatus": true,
+          "hideSymbolLogo": false,
+          "scalePosition": "right",
+          "scaleMode": "Normal",
+          "fontFamily": "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
+          "fontSize": "10",
+          "noTimeScale": false,
+          "valuesTracking": "1",
+          "changeMode": "price-and-percent",
+          "chartType": "area",
+          "lineWidth": 2,
+          "lineType": 0,
+          "dateRanges": [
+            "1d|1",
+            "1m|1D",
+            "3m|60",
+            "12m|1D",
+            "60m|1W",
+            "all|1M"
+          ]
+        } `;
+    container.current.appendChild(script);
   }, []);
 
   return (
-    <div className="tradingview-widget-container pt-2">
+    <div className="tradingview-widget-container" ref={container}>
       <div className="tradingview-widget-container__widget"></div>
       <div className="tradingview-widget-copyright">
         <a
@@ -125,7 +65,7 @@ export const TradingGrap1 = () => {
           rel="noopener nofollow"
           target="_blank"
         >
-          {/* <span className="blue-text">Track all markets on TradingView</span> */}
+          <span className="blue-text">Track all markets on TradingView</span>
         </a>
       </div>
     </div>
