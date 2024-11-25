46de39d973cca4d8018d97556bd6b7e6d1d18e74
Oluwatobi97 <victortobi2000@gamil.com>
2024-11-25 21:09:22 +0100

    chat

--- src/views/TradingView/TradingView.jsx
+++ src/views/TradingView/TradingView.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { TradingGrap } from "../auth/shared/AboutUi/TradingGrap";
 import { TradingGrap1 } from "../auth/shared/AboutUi/TradingGrap1";
+import { TradingViewTickerTape } from "../auth/shared/AboutUi/TradingViewTickerTape";
 
 export const TradingView = () => {
   return (
     <div>
       <TradingGrap />
+      <TradingViewTickerTape className={"pt-2"} />
       <TradingGrap1 />
     </div>
   );
---
+++ src/views/auth/shared/AboutUi/TradingViewTickerTape.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect } from "react";
+
+export const TradingViewTickerTape = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src =
+      "https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js";
+    script.async = true;
+
+    // Widget configuration
+    script.innerHTML = JSON.stringify({
+      symbols: [
+        {
+          proName: "BITSTAMP:BTCUSD",
+          title: "Bitcoin",
+        },
+        {
+          description: "Bitcoin",
+          proName: "BINANCE:BTCUSDT",
+        },
+        {
+          description: "ETH",
+          proName: "BINANCE:ETHUSDT",
+        },
+        {
+          description: "DOG",
+          proName: "BINANCE:DOGEUSDT",
+        },
+        {
+          description: "SOL",
+          proName: "BINANCE:SOLUSDT",
+        },
+      ],
+      showSymbolLogo: true,
+      isTransparent: false,
+      displayMode: "compact",
+      colorTheme: "light",
+      locale: "en",
+    });
+
+    // Append the script to the container
+    const container = document.querySelector(
+      ".tradingview-widget-container__widget"
+    );
+    container.appendChild(script);
+
+    // Cleanup script on component unmount
+    return () => {
+      container.innerHTML = "";
+    };
+  }, []);
+
+  return (
+    <div className="tradingview-widget-container">
+      <div className="tradingview-widget-container__widget"></div>
+      <div className="tradingview-widget-copyright">
+        <a
+          href="https://www.tradingview.com/"
+          rel="noopener nofollow"
+          target="_blank"
+        >
+          <span className="blue-text">Track all markets on TradingView</span>
+        </a>
+      </div>
+    </div>
+  );
+};
