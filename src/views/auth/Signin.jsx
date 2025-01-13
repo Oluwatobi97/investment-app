b45d2c5ad748abc58bf32fa82bbe39e7bad944e8
Ktbch <140395978+Ktbch@users.noreply.github.com>
2025-01-13 10:04:51 +0100

    Update Signin.jsx
    removal of uncessary code

--- src/views/auth/Signin.jsx
+++ src/views/auth/Signin.jsx
@@ -37,9 +37,6 @@ const useSignInMutation = () => {
       if (data.status === 200) {
         authenticate(data.token)
       }
-      if (data.status === 401) {
-        setToast({ message: 'Invalid username or password', status: 'error' })
-      }
     },
     onError: async error => {
       setToast({
