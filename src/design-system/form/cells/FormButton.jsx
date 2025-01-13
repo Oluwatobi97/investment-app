d314a4b586af8cfaebb2765a7211d266324e36ee
Ktbch <odufotekayode4@gmail.com>
2025-01-13 15:13:25 +0100

    auth/toat fix

--- src/design-system/form/cells/FormButton.jsx
+++ src/design-system/form/cells/FormButton.jsx
@@ -3,15 +3,9 @@ import { LoaderCmp } from '../../../components/LoaderCmp'
 
 export const FormButton = ({ buttonName, className, loading }) => {
   return (
-    <button
-      type='submit'
-      className={!className ? '' : className}
-      disabled={loading}
-		>
-      <span className={`${loading ? ' ' : ''}`}>
-        {loading ? <LoaderCmp /> : buttonName}
-      </span>
-    </button>
+    <motion.button type='submit' className={className} disabled={loading}>
+      {mutate.isPending ? <span>Loading...</span> : buttonName}
+    </motion.button>
   )
 }
 
