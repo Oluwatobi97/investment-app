a7023a3fecd75e2998784885860e0a3f4cf3a4d7
Ktbch <odufotekayode4@gmail.com>
2025-01-13 15:32:29 +0100

    bug fix of undefined mutate

--- src/design-system/form/cells/FormButton.jsx
+++ src/design-system/form/cells/FormButton.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion'
 export const FormButton = ({ buttonName, className, loading }) => {
   return (
     <motion.button type='submit' className={className} disabled={loading}>
-      {mutate.isPending ? <span>Loading...</span> : buttonName}
+      {loading ? <span>Loading...</span> : buttonName}
     </motion.button>
   )
 }
